2052a374a797a021835bd96cdbb27d5becbb8c20
Lageder <rageder01@gmail.com>
2022-11-23 17:28:38 +0900

    - Add missing todo list which we should do after someday

--- src/main/java/presentation/VRUI.java
+++ src/main/java/presentation/VRUI.java
@@ -14,6 +14,7 @@ import java.util.List;
 	todo list
 	- Can we process customers and videos information in another class?
 	- Separate init responsibility
+	- Apply command pattern to reduce CC in main method
 * */
 public class VRUI {
 	Logger logger = new Logger();
